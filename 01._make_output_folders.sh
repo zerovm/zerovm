791e9a212bba75bccc05d54757f1bfcc00535955
camuel <camuel@bigdatacraft.com>
2011-11-26 00:11:31 +0700

    2011-09-29 linux, x86, 64 working_sandbox_ver_2.7z version without syscalls --------------------------------------------------------------------------------     2nd version of sandbox. some of syscalls still left untouched because they work in process of load/exit nexe.     without them (or part of them) nexe cannot be loaded and cannot be run (for example we will always get     "segmentation fault" w/o exit syscall)
        there are no binaries in the archive. only sources and scripts
    
        sel_ldr - compiled sandbox. this version differ from the original
    (most of syscalls are absent)
    
        building scripts are fixed. extra "-I" options were removed. all
    absolute pathes changed to relative.
        all libraries will be placed to one temporary folder. after sandbox
    compilation all intermediate
        files (*.a, *.o) will be deleted.     script "build_all.sh" was
    added (now whole build process can
        be done in one keystroke).
    
        todo:
    
        yet more simplify directory structure (1st of all completely remove
    "scons_out" and make one folder for intermediate files)
    
        to solve problem with warnings (syscalls were not completely
    removed, just disables). now i see
        warnings "defined but not used" due compilation.
    "02._compile_all_sources.sh" should be changed.
        if solved i have to put back "-Werror" option (treat warnings as
    errors).
    
        to remove "step 1": "01._make_output_folders.sh" since we need
    additional folders no more (except only one)

---
+++ 01._make_output_folders.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#SHOULD BE RUN FROM THE "SANDBOX ROOT FOLDER"
+#----------------------------------------------------------
+
+mkdir -p lib
+mkdir -p native_client/scons-out/opt-linux-x86-64/gen/native_client/src/trusted/service_runtime
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/shared/gio
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/shared/imc
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/shared/imc/linux
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/shared/platform
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/shared/platform/linux
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/shared/srpc
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/debug_stub
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/debug_stub/posix
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/desc
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/desc/linux
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/desc/pepper
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/desc/posix
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/fault_injection
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/gdb_rsp
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/gio
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/manifest_name_service_proxy
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/nacl_base
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/perf_counter
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/platform_qualify
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/platform_qualify/arch/x86
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/platform_qualify/arch/x86_64
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/platform_qualify/linux
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/platform_qualify/posix
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/service_runtime
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/service_runtime/arch/x86
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/service_runtime/arch/x86_64
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/service_runtime/generic_container
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/service_runtime/linux
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/service_runtime/linux/x86
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/service_runtime/name_service
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/service_runtime/posix
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/simple_service
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/threading
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/validator/x86
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/validator/x86/64
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/validator/x86/decoder
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/validator/x86/ncval_reg_sfi
+mkdir -p native_client/scons-out/opt-linux-x86-64/obj/src/trusted/validator_x86
