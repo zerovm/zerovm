b4a1fe5295212f15fdadcca953e733f16e752edc
camuel <camuel@bigdatacraft.com>
2011-12-22 19:08:29 +0700

    build scripts fixes

--- api/compile_api.sh
+++ api/compile_api.sh
@@ -1,1 +1,1 @@
-~/nacl/native_client/toolchain/linux_x86/bin/x86_64-nacl-gcc -c -Wall -c -Wno-long-long -O2 -msse4.1 -m64 -I~/sandbox/ -I ~/nacl/native_client/toolchain/linux_x86/x86_64-nacl/include $1
+~/git/nacl_sdk/pepper_15/toolchain/linux_x86/bin/x86_64-nacl-gcc -c -Wall -c -Wno-long-long -O2 -msse4.1 -m64 -I~/sandbox/ -I ~/git/nacl_sdk/pepper_15/toolchain/linux_x86/x86_64-nacl/include $1
--- mknexe.sh
+++ mknexe.sh
@@ -1,6 +1,4 @@
-~/nacl/native_client/toolchain/linux_x86/bin/x86_64-nacl-gcc -o killme.o -DUSER_SIDE -Wall -c -Wno-long-long -O2 -msse4.1 -m64 -iquote/home/dazo/git/zerovm/api -I/home/dazo/nacl/native_client/toolchain/linux_x86/x86_64-nacl/include $1
-
-~/nacl/native_client/toolchain/linux_x86/bin/x86_64-nacl-gcc -o $1_x86_64.nexe -s -static -T ~/nacl/native_client/toolchain/linux_x86/x86_64-nacl/lib/ldscripts/elf64_nacl.x.static -melf64_nacl -m64 killme.o ~/git/zerovm/api/zvm.o -lc
-
+~/git/nacl_sdk/pepper_15/toolchain/linux_x86/bin/x86_64-nacl-gcc -o killme.o -DUSER_SIDE -Wall -c -Wno-long-long -O2 -msse4.1 -m64 -iquote/home/dazo/git/zerovm/api -I/home/dazo/nacl/native_client/toolchain/linux_x86/x86_64-nacl/include $1
+~/git/nacl_sdk/pepper_15/toolchain/linux_x86/bin/x86_64-nacl-gcc -o $1_x86_64.nexe -s -static -T ~/git/nacl_sdk/pepper_15/toolchain/linux_x86/x86_64-nacl/lib/ldscripts/elf64_nacl.x.static -melf64_nacl -m64 killme.o ~/git/zerovm/api/zvm.o -lc
 rm killme.o
 
