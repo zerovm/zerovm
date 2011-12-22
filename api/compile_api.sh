d272781805d69df773542ef14bbb161fbd49d563
bortoq <bortoq@gmail.com>
2011-12-22 18:18:01 +0700

    merge

---
+++ api/compile_api.sh
@@ -0,0 +1,1 @@
+~/nacl/native_client/toolchain/linux_x86/bin/x86_64-nacl-gcc -c -Wall -c -Wno-long-long -O2 -msse4.1 -m64 -I~/sandbox/ -I ~/nacl/native_client/toolchain/linux_x86/x86_64-nacl/include $1
