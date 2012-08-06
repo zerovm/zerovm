16b83a193874699d052b9e2d9ca51ee7670b5b46
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2012-08-07 02:14:05 +0300

    reqrep script

--- samples/reqrep/test.sh
+++ samples/reqrep/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gnome-terminal --geometry=80x20 -t "zerovm test1.manifest" -x sh -c "../../zerovm -Mtest1.manifest -v10"
-../../zerovm -Mtest2.manifest
+gnome-terminal --geometry=80x20 -t "zerovm test1.manifest" -x sh -c "../../zerovm -Mtest2.manifest"
+../../zerovm -Mtest1.manifest -v10
 
 sleep 1
 echo "############### test 1 #################"
