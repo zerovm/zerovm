c9190dc94df110c9bdd2d239f5605d0d5e5e73a1
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2012-10-07 15:32:22 +0200

    sample reproduces mismatch of heap & stack

--- src/tests/functional/glibc_bug/glibc_bug.c
+++ src/tests/functional/glibc_bug/glibc_bug.c
@@ -13,24 +13,56 @@
 #include "../include/api_tools.h"
 
 #define BIG_ENOUGH 0x10000
+#define STACK_SIZE   0x1000000
+
+static char s_msg[BIG_ENOUGH];
 
 /* We are substitute glibc POSIX function to our defined function */
 int mkdir(const char* pathname, mode_t mode)
 {
-  char msg[BIG_ENOUGH];
-
   ZTEST(pathname == NULL);
-  snprintf(msg, BIG_ENOUGH,
+  snprintf(s_msg, BIG_ENOUGH,
       "pathname = %X, sizeof pathname = %d, sizeof(uintptr_t) = %d\n",
       (uintptr_t)pathname, sizeof pathname, sizeof(uintptr_t));
-  zput(STDLOG, msg);
-
+  zput(STDLOG, s_msg);
   return 0;
+}
+
+uint32_t check_stack(){
+    snprintf(s_msg, BIG_ENOUGH,
+      "user heap pointer address = 0x%x\n", (intptr_t)zvm_bulk->heap_ptr);
+    zput(STDLOG, s_msg);
+
+    snprintf(s_msg, BIG_ENOUGH,
+      "user memory size = %u\n", zvm_bulk->mem_size);
+    zput(STDLOG, s_msg);
+
+    if ( zvm_bulk->heap_ptr ){
+        intptr_t heap_end_address = (intptr_t)zvm_bulk->heap_ptr+zvm_bulk->mem_size;
+        intptr_t stack_bound = 0xFFFFFFFF-STACK_SIZE;
+
+        snprintf(s_msg, BIG_ENOUGH,
+          "calculated heap end address= 0x%x, stack bound=0x%X\n", heap_end_address, stack_bound );
+        zput(STDLOG, s_msg);
+
+        if ( stack_bound < heap_end_address ){
+            return heap_end_address - stack_bound;
+        }
+    }
+    return 0;
 }
 
 int main(int argc, char **argv)
 {
   zvm_bulk = zvm_init();
+
+  uint32_t overlap = check_stack();
+  if ( overlap != 0 ){
+      snprintf(s_msg, BIG_ENOUGH,
+        "heap and stack overlaps on 0x%X bytes\n", overlap );
+      zput(STDLOG, s_msg);
+  }
+
   mkdir(NULL, 0xEEEEEE);
 
   zvm_exit(0);
