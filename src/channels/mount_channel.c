90468b9c0ada76e18caea07622a07c725af27d93
bortoq <bortoq@gmail.com>
2013-01-16 16:02:05 -0600

    #92 errno code changed to more appropriate

--- src/channels/mount_channel.c
+++ src/channels/mount_channel.c
@@ -213,7 +213,7 @@ void ChannelsCtor(struct NaClApp *nap)
   mft = nap->system_manifest;
   mft->channels_count =
       GetValuesByKey("Channel", values, MAX_CHANNELS_NUMBER);
-  ZLOGFAIL(mft->channels_count >= MAX_CHANNELS_NUMBER, EFAULT,
+  ZLOGFAIL(mft->channels_count >= MAX_CHANNELS_NUMBER, ENFILE,
       "channels number reached maximum");
   ZLOGFAIL(mft->channels_count < RESERVED_CHANNELS, EFAULT,
       "not all standard channels are provided");
