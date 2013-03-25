93eecb145c9d0f942ffa8d7c7c9be77b67b47005
bortoq <bortoq@gmail.com>
2013-03-25 17:55:07 -0500

    #102 functional manifest tests added

---
+++ tests/functional/manifest/test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+for i in `find *.manifest`; do
+  zerovm $i > /dev/null
+  if [ $? = 0 ]; then
+    echo "\033[01;38m$i\033[00m test has \033[02;31mfailed\033[00m"
+  else
+    echo "\033[01;38m$i\033[00m test has \033[01;32mpassed\033[00m"
+  fi
+done
